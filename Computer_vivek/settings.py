9ba7feb6b252ac64fbc09d63e34cef18dffbab3a
viralsir <viralsir2018@gmail.com>
2021-08-10 11:37:40 +0530

    login and logout functionality added.

--- Computer_vivek/settings.py
+++ Computer_vivek/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'users',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -125,3 +126,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL='user-home'
--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from users.views import home
+from django.contrib.auth.views import LoginView,LogoutView
 urlpatterns=[
-    path("home/",home,name="user-home")
+    path("home/",home,name="user-home"),
+    path("login/",LoginView.as_view(template_name="users/Login.html"),name="login"),
+    path("logout/",LogoutView.as_view(template_name="users/logout.html"),name="logout")
+
 ]
